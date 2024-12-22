07484af534074687927a2befd382ac9abe9e74ee
giorgia <s332968@studenti.polito.it>
2024-12-22 11:40:12 +0100

    ???

--- code/dataset.py
+++ code/dataset.py
@@ -1,8 +1,17 @@
 from torch.utils.data import DataLoader
+from LoveDA_class import LoveDADataset
+import torchvision.transforms as T
 
 # Percorsi al dataset
 train_images_dir = "/path/to/LoveDA/Train/Images"
 train_masks_dir = "/path/to/LoveDA/Train/Annotations"
+
+transform = T.Compose([
+    T.Resize((512, 512)),  # Ridimensiona immagini e maschere
+    T.ToTensor(),          # Converte in tensore
+    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalizzazione
+])
+
 
 # Dataset e DataLoader
 train_dataset = LoveDADataset(images_dir=train_images_dir, masks_dir=train_masks_dir, transform=transform)
---
+++ data/possible_dataset_structure.md
@@ -0,0 +1,13 @@
+LoveDA/
+├── Train/
+│   ├── Images/
+│   │   ├── image1.png
+│   │   ├── image2.png
+│   └── Annotations/
+│       ├── image1_label.png
+│       ├── image2_label.png
+├── Val/
+│   ├── Images/
+│   └── Annotations/
+├── Test/
+    ├── Images/
